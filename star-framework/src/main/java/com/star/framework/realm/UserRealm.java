bbc93b7539e8ebe5a9f131a9e14f2f2987488ca1
zut-cs-427 <859193125@qq.com>
2022-01-07 15:34:27 +0800

    整合shiro

---
+++ star-framework/src/main/java/com/star/framework/realm/UserRealm.java
@@ -0,0 +1,42 @@
+package com.star.framework.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+
+/**
+ * 自定义 Realm
+ */
+public class UserRealm extends AuthorizingRealm {
+
+
+    /**
+     * 授权
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        //创建授权对象
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * 认证
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+        return null;
+    }
+}
