224e8bbe7fb75463d1a841bb9625340bb5312dc9
zut-cs-427 <859193125@qq.com>
2022-01-07 15:02:00 +0800

    配置mybatisPlus分页插件

---
+++ star-framework/src/main/java/com/star/framework/config/MyBatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.star.framework.config;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/***
+ * Mybatis的配置类
+ */
+@Configuration//标记该类是一个配置类
+@EnableTransactionManagement//开启事务管理
+// @MapperScan(basePackages = {"com.star.*.dao"})
+public class MyBatisPlusConfig {
+
+    // 最新版
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+}
