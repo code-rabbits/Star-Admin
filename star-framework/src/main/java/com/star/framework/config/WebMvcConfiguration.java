d7f121ba6fa9126ddd88bb3ef437a089d46e439c
zut-cs-427 <859193125@qq.com>
2022-01-07 15:04:44 +0800

    自定义WebMvc配置

---
+++ star-framework/src/main/java/com/star/framework/config/WebMvcConfiguration.java
@@ -0,0 +1,20 @@
+package com.star.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 自定义WebMvc配置
+ */
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //去到登录页面请求，浏览器访问以下请求，都是去到登录页面
+        registry.addViewController("/sys/login").setViewName("login");
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login.html").setViewName("login");
+    }
+
+}
