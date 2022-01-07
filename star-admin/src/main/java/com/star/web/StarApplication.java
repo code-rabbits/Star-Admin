82e238021afb0ee7d09147c6b22efc65577c902f
zut-cs-427 <859193125@qq.com>
2022-01-07 12:17:33 +0800

    创建项目

---
+++ star-admin/src/main/java/com/star/web/StarApplication.java
@@ -0,0 +1,16 @@
+package com.star.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ *
+ * @author
+ */
+@SpringBootApplication
+public class StarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StarApplication.class, args);
+    }
+}
